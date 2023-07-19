fc5e3a9256a99c5112c2d2bde2e2ddb734b90d2f
Jahedul Hoque <jahedulh1@gmail.com>
2023-07-19 08:26:50 +0200

    some issue updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,33 @@
-import App from '@/App'
-import Home from '@/pages/Home'
-import Login from '@/pages/Login'
-import NotFound from '@/pages/NotFound'
-import Register from '@/pages/Register'
-import { createBrowserRouter } from 'react-router-dom'
+import App from '@/App';
+import Home from '@/pages/home';
+import Login from '@/pages/login';
+import NotFound from '@/pages/not-found';
+import Register from '@/pages/register';
+import { createBrowserRouter } from 'react-router-dom';
 
 const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children: [
-            {
-                index: true,
-                element: <Home/>
-            }
-        ]
-    },
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/register",
-        element: <Register/>
-    },
-    {
-        path: "*",
-        element: <NotFound/>
-    },
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
 ]);
 
 export default routes;
